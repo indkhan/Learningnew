385cb29e9995032e1c75e1405fe46dc1ffa558ed
Usman Khan <mgsukhan@gmail.com>
2024-04-29 14:14:58 +0300

    more

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,23 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  console.log(__dirname + "/public/index.html"),
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(req.body.street+" " +req.body.pet);
+})
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
