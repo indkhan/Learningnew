07e276c970ef604f4adc9e908d0805c89d3f317b
Usman Khan <mgsukhan@gmail.com>
2024-07-01 11:55:25 +0530

    adding promptmania

---
+++ Nike clone/src/sections/index.js
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+
+
+export { Hero, CustomerReviews, SuperQuality, Footer, Subscribe, PopularProducts, Services, SpecialOffer }
