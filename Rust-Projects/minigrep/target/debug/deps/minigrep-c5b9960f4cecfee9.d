a3a7b44fd631c4619c60e310fd796a8aecdcc5f0
Usman Khan <mgsukhan@gmail.com>
2024-07-21 22:30:09 +0530

    starting with minigrep

---
+++ Rust-Projects/minigrep/target/debug/deps/minigrep-c5b9960f4cecfee9.d
@@ -0,0 +1,5 @@
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\minigrep\target\debug\deps\libminigrep-c5b9960f4cecfee9.rmeta: src/main.rs
+
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\minigrep\target\debug\deps\minigrep-c5b9960f4cecfee9.d: src/main.rs
+
+src/main.rs:
---
+++ Rust-Projects/minigrep/target/debug/deps/minigrep-f057916265d4d09a.d
@@ -0,0 +1,5 @@
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\minigrep\target\debug\deps\libminigrep-f057916265d4d09a.rmeta: src/main.rs
+
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\minigrep\target\debug\deps\minigrep-f057916265d4d09a.d: src/main.rs
+
+src/main.rs:
