bafd1514ae712ded32755eea0e43b2d64eea07d3
Usman Khan <mgsukhan@gmail.com>
2024-07-22 23:50:36 +0530

    checkig

--- Rust-Projects/src/main.rs
+++ Rust-Projects/src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     println!("what is ur name!");
     io::stdin().read_line(&mut input).expect("Input Failed");
     println!("Good Morning :{}", input);
+
+    let v = vec![1, 2, 3];
+    let iter = v.iter();
+    println!(iter)
 }
--- Rust-Projects/target/debug/deps/Rusti.d
+++ Rust-Projects/target/debug/deps/Rusti.d
@@ -1,5 +1,5 @@
-C:\Users\mgsuk\OneDrive\Desktop\Learningnew\Rusti\target\debug\deps\Rusti.exe: src/main.rs
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\target\debug\deps\Rusti.exe: src/main.rs
 
-C:\Users\mgsuk\OneDrive\Desktop\Learningnew\Rusti\target\debug\deps\Rusti.d: src/main.rs
+C:\Users\mgsuk\OneDrive\Desktop\learningnew\rust-projects\target\debug\deps\Rusti.d: src/main.rs
 
 src/main.rs:
