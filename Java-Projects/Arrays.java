63e345782f041f3a75b12bf7c4a605ae58523317
Usman Khan <mgsukhan@gmail.com>
2024-08-09 00:10:01 +0300

    1 and 2 d arrays

---
+++ Java-Projects/Arrays.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Arrays {
+    public static void main(String args[]) {
+        System.out.println("enter the size of the array you want");
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        for (int x = 0; x < size; x++) {
+            System.out.println("Enter the integer for the array");
+
+            int num = sc.nextInt();
+            numbers[x] = num;
+
+        }
+        
+        int y = 4;
+        for(int i=0; i<size; i++) {
+            if(numbers[i] == y) {
+
+                System.out.println("found at index : "+i);
+                
+            }}
+
+
+
+        for(int i=0; i<size; i++) {
+            System.out.println(numbers[i]);
+        }
+        System.out.println("the length is : "+numbers.length);
+    }
+}
